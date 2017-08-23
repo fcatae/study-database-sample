1ac25cdccb16b071155ec15bb5418bad24be93a5
Fabricio Catae <fcatae@microsoft.com>
2017-08-22 22:10:35 -0300

    fix address

--- schema/address.sql
+++ schema/address.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [Address]
 (
-	[AddressID]	    [int] IDENTITY(1,1) NOT NULL,
+	[AddressID]	    [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[AddressLine1]  [nvarchar](60)		NOT NULL,
 	[AddressLine2]  [nvarchar](60)		NULL,
 	[City]	        [nvarchar](30)		NOT NULL,
