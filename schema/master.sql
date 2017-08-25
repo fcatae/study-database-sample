2cbbddffd6e13408cd2cc230422e8458159f34e3
Fabricio Catae <fcatae@microsoft.com>
2017-08-25 11:02:10 -0300

    update master doc

--- schema/master.sql
+++ schema/master.sql
@@ -1,10 +1,23 @@
-select * from sys.sql_logins
-select * from sys.database_principals
-select * from sys.databases
-select * from sys.elastic_pool_resource_stats
+-- Create Co-Admin
+CREATE USER admsql WITH PASSWORD=''
+ALTER ROLE loginmanager ADD MEMBER admsql
+SELECT * FROM sys.database_principals
+SELECT * FROM sys.database_role_members
 
+-- Create Logins
+CREATE LOGIN [u09] WITH PASSWORD=''
+SELECT * FROM sys.sql_logins
+
+-- Create Databases
 CREATE DATABASE DB10 AS COPY OF [LabModel]
-select state_desc,* from sys.databases WHERE name='DB10'
-select * from sys.dm_database_copies
+SELECT * FROM sys.databases
 
-create login u09 with password=''
+SELECT state_desc,* FROM sys.databases WHERE name='DB10'
+SELECT * FROM sys.dm_database_copies
+
+-- Inside database
+-- ALTER AUTHORIZATION ON database::DB10 TO u04
+
+-- Monitor resources
+select * from sys.elastic_pool_resource_stats
+select * from sys.resource_stats
