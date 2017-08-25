9f0a978d5ba46bdb216d695e3bee9fd002637623
Fabricio Catae <fcatae@microsoft.com>
2017-08-25 11:17:41 -0300

    update master db

--- schema/masterdb.sql
+++ schema/masterdb.sql
@@ -1,32 +1,26 @@
-select * from sys.sql_logins
-select * from sys.database_principals
-select * from sys.databases
-select * from sys.elastic_pool_resource_stats
-
-EXEC('create database DB09 as copy of LabModel')
+EXEC('create database DB10 as copy of LabModel')
 -- select * from sys.dm_database_copies
 
-DECLARE @pwd VARCHAR(100)
-EXEC('create login u08 with password=''' + @pwd + '''')
+DECLARE @pwd VARCHAR(100) = ''
+EXEC('create login u09 with password=''' + @pwd + '''')
 
 -- MASTERDB
 CREATE DATABASE MASTERDB
 ALTER DATABASE MASTERDB
-modify (SERVICE_OBJECTIVE=ELASTIC_POOL(NAME=DBS))
+MODIFY (SERVICE_OBJECTIVE=ELASTIC_POOL(NAME=DBS))
 
 CREATE MASTER KEY;
 CREATE DATABASE SCOPED CREDENTIAL LabDataCredential
-...
-
-drop PROC setupDataSource
-go
-CREATE PROC setupDataSource(@servername VARCHAR(30), @dbname VARCHAR(30))
+WITH IDENTITY='', SECRET=''
+GO
+CREATE OR ALTER PROC setupDataSource(@servername VARCHAR(30), @dbname VARCHAR(30))
 AS
+DECLARE @source VARCHAR(30) = 'LabData' + @dbname
 DECLARE @server VARCHAR(30) = @servername;
 DECLARE @db VARCHAR(30) =  @dbname;
 
 	EXEC('
-	CREATE EXTERNAL DATA SOURCE LabData
+	CREATE EXTERNAL DATA SOURCE ' + @source + '
 	WITH (
 		TYPE = RDBMS, 
 		LOCATION = ''' + @server + '.database.windows.net'', 
@@ -36,20 +30,24 @@ DECLARE @db VARCHAR(30) =  @dbname;
 	);
 GO
 
-EXEC setupDataSource 'sqlfcatae', 'DB08'
+EXEC setupDataSource 'sqlfcatae', 'DB09'
 drop external data source LabData
 
 select * from sys.external_data_sources
 
 -- setup user with low privilege
-sp_execute_remote N'LabData', 
+declare @source NVARCHAR(30) = N'LabData' + 'DB09'
+exec sp_execute_remote @source, 
 		N'setupUser @loginname, @username', 
 		N'@loginname VARCHAR(50), @username VARCHAR(50)',
-		'u04',
-		'dev'
+		'u08',
+		'u08'
 
 -- List visible objects
-sp_execute_remote N'LabData', 
+declare @source NVARCHAR(30) = N'LabData' + 'DB09'
+exec sp_execute_remote @source, 
+		N'sp_tables'
+exec sp_execute_remote @source, 
 		N'select * from sys.objects'
 
 -- List visible objects
@@ -59,10 +57,11 @@ sp_execute_remote N'LabData',
 		'u08',
 		'u08'
 
-sp_execute_remote N'LabData', 
+declare @source NVARCHAR(30) = N'LabData' + 'DB09'
+exec sp_execute_remote @source, 
 		N'setupRunAs @username',
 		N'@username VARCHAR(50)',
-		'dev'
+		'u08'
 
 CREATE PROC setupRunAs(@username VARCHAR(50))
 WITH EXECUTE AS OWNER
